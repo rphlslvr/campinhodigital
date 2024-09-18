b263295fa1c2e9d3da1d8e626a753e2c00d46f12
rphlslvr <rphlslvr@gmail.com>
2024-09-18 23:21:57 +0000

    atualizando exercicios

---
+++ collections.py
@@ -0,0 +1,44 @@
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(" ")
+
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+print(" ")
+
+myFruitList[2] = "orange"
+print(myFruitList)
+
+print(" ")
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(" ")
+
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+print(" ")
+
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+print(" ")
+
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
+
